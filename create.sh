5acf5bc3520e256978b938df04246e0a89e7b3b5
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-08-02 22:00:18 +0100

    Infrastructure as code

--- create.sh
+++ create.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name Udagram --template-body file://infrastructure.yml    --parameters file://network-parameters.json  --region=us-east-1
+aws cloudformation create-stack --stack-name Udagram --template-body file://network-infrastructure.yml    --parameters file://network-parameters.json  --region=us-east-1
