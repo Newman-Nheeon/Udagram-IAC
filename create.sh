9021b0d80c6f0799bdbc852cb5347f74f9a69a1c
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-08-05 16:54:03 +0100

    Udagram IAC

--- create.sh
+++ create.sh
@@ -1,1 +1,2 @@
-aws cloudformation create-stack --stack-name Udagram --template-body file://network-infrastructure.yml    --parameters file://network-parameters.json  --region=us-east-1
+aws cloudformation create-stack --stack-name Udagram --template-body file://network-infrastructure.yml    --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name Udagram-servers --template-body file://servers-infrastructure.yml    --parameters file://servers-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"  --region=us-east-1
